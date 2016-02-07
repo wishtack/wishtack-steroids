aa05ace93deebc5ff4e4758f85f57e6ca49b5d2e
Younes JAAIDI <yjaaidi@gmail.com>
2016-02-07 20:29:46 +0100

    WIP: Build JS and inject it in HTML file.

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,7 @@
 
 var _ = require('lodash');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+var glob = require('glob');
 var walkSync = require('walk-sync');
 var webpack = require('webpack');
 
@@ -15,15 +17,26 @@ module.exports = {
         return fileName.replace(/\.d\.ts$/, '');
     }),
     output: {
-        filename: 'dist/assets/scripts/app.js',
-        libraryTarget: 'umd'
+        filename: 'assets/scripts/app.[chunkhash].js',
+        libraryTarget: 'umd',
+        path: 'dist',
+        publicPath: '/'
     },
     devtool: 'source-map',
     resolve: {
         extensions: ['', '.webpack.js', '.web.js', '.ts', '.js']
     },
     plugins: [
-        new webpack.optimize.UglifyJsPlugin()
+        new webpack.optimize.UglifyJsPlugin(),
+        new HtmlWebpackPlugin({
+            filename: 'templates/home.html',
+            minify: {
+                collapseWhitespace: true,
+                removeComments: true
+            },
+            template: 'app/templates/home.html',
+            inject: 'head'
+        })
     ],
     module: {
         loaders: [
