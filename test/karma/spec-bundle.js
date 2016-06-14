8f5da03b251b65c68bd2580ad937205868f76a64
Younes JAAIDI <yjaaidi@gmail.com>
2016-06-14 13:24:29 +0200

    Added mising require 'zone.js/dist/async-test'.

--- test/karma/spec-bundle.js
+++ test/karma/spec-bundle.js
@@ -20,6 +20,7 @@ Error.stackTraceLimit = Infinity;
 require('core-js/es6');
 require('core-js/es7/reflect');
 require('zone.js/dist/zone');
+require('zone.js/dist/async-test');
 require('zone.js/dist/fake-async-test');
 require('zone.js/dist/sync-test');
 require('@angular/platform-browser-dynamic');
