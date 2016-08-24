b474c90072c127beb8a560c6c25b27106c5766b5
Younes JAAIDI <yjaaidi@gmail.com>
2016-08-24 18:45:48 +0200

    Removed angular 2 code.

--- app/angular/app/app.module.ts
+++ app/angular/app/app.module.ts
@@ -7,9 +7,18 @@
 
 
 import * as angular from 'angular';
+<<<<<<< ebb98802b9050d72639a308d573dc45ae8658c65
 import {UserStore} from './common/user/user-store';
 import {userListModule} from './user-list/user-list.module';
 
 export const appModule = angular.module('app', [
     userListModule.name
 ]);
+=======
+import {AppComponent} from './app.component';
+
+export const name = 'app';
+export const app = angular.module(name, []);
+
+app.component(AppComponent.name, AppComponent);
+>>>>>>> Removed angular 2 code.
