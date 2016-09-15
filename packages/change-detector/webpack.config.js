b90097471cde829e7d5f50688fe70ee637f7f397
Younes JAAIDI <yjaaidi@gmail.com>
2016-09-16 01:15:44 +0200

    Changed entry point.

--- packages/change-detector/webpack.config.js
+++ packages/change-detector/webpack.config.js
@@ -10,7 +10,7 @@ const path = require('path');
 const WebpackConfigFactory = require('../../config/webpack-config-factory').WebpackConfigFactory;
 
 module.exports = new WebpackConfigFactory().buildConfig({
-    entry: path.join(__dirname, 'src/index.ts'),
+    entry: path.join(__dirname, 'src/change-detector.module.ts'),
     libraryName: 'change-detector',
     outputPath: path.join(__dirname, 'dist'),
     srcRootPath: path.join(__dirname, 'src')
