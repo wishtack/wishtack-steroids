a0b718a5fe2b3334dcd30a2f3ecf7d9197c1515b
Younes JAAIDI <yjaaidi@gmail.com>
2016-09-15 21:19:48 +0200

    Config fatorization.

--- config/webpack-config-factory.js
+++ config/webpack-config-factory.js
@@ -6,49 +6,47 @@
  */
 
 var webpack = require('webpack');
+var webpackMerge = require('webpack-merge');
 var path = require('path');
 
 class WebpackConfigFactory {
 
     buildConfig({entry, libraryName, outputPath, srcRootPath}) {
-        return {
-            entry: entry,
-            devtool: 'source-map',
-            output: {
-                path: outputPath,
-                filename: `${libraryName}.min.js`,
-                library: libraryName,
-                libraryTarget: 'umd',
-                umdNamedDefine: true
-            },
-            module: {
-                loaders: [
-                    {
-                        test: /\.ts$/,
-                        loader: 'babel!ts',
-                        exclude: /node_modules/
-                    },
-                    {
-                        test: /\.js$/,
-                        loader: 'babel',
-                        exclude: /node_modules/
-                    }
+
+        return webpackMerge(
+            this._commonConfig({srcRootPath}),
+            {
+                entry: entry,
+                devtool: 'source-map',
+                output: {
+                    path: outputPath,
+                    filename: `${libraryName}.min.js`,
+                    library: libraryName,
+                    libraryTarget: 'umd',
+                    umdNamedDefine: true
+                },
+                plugins: [
+                    new webpack.optimize.UglifyJsPlugin({minimize: true})
                 ]
-            },
-            resolve: {
-                root: srcRootPath,
-                extensions: ['', '.js', '.ts']
-            },
-            plugins: [
-                new webpack.optimize.UglifyJsPlugin({ minimize: true })
-            ]
-        };
+            }
+        );
+
     }
 
     testConfig({srcRootPath}) {
 
+        return webpackMerge(
+            this._commonConfig({srcRootPath}),
+            {
+                devtool: 'inline-source-map'
+            }
+        );
+
+    }
+
+    _commonConfig({srcRootPath}) {
+
         return {
-            devtool: 'inline-source-map',
             module: {
                 loaders: [
                     {
@@ -67,7 +65,7 @@ class WebpackConfigFactory {
                 root: srcRootPath,
                 extensions: ['', '.js', '.ts']
             }
-        };
+        }
 
     }
 
