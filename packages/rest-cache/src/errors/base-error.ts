f5f617d3dfae3d5e9160de1162474d977de2f11b
Younes JAAIDI <yjaaidi@gmail.com>
2017-01-27 20:08:53 +0100

    Removed stupid hacks as `BaseError` doesn't have to inherit from `Error`.

--- packages/rest-cache/src/errors/base-error.ts
+++ packages/rest-cache/src/errors/base-error.ts
@@ -5,47 +5,16 @@
  * $Id: $
  */
 
+export class BaseError {
 
-const FixErrorPrototype = () => {
+    constructor(public message: string) {}
 
-    return (klass) => {
-
-        /* Override constructor. */
-        const newKlass: any = function (...argList) { /* tslint:disable-line */
-
-            /* Call original constructor. */
-            klass.apply(this, argList);
-
-        };
-
-        /* https://github.com/Microsoft/TypeScript-wiki/blob/master/Breaking-Changes.md
-         * #extending-built-ins-like-error-array-and-map-may-no-longer-work.
-         * @hack: This hack prevents TypeScript from applying `Error` prototype to our class. */
-        newKlass.prototype = klass.prototype;
-
-        return newKlass;
-
-    };
-
-};
-
-@FixErrorPrototype()
-export class BaseError extends Error {
-
-    private _nativeError: Error;
-
-    constructor(message: string) {
-        /* Cf. https://github.com/angular/angular => angular/modules/@angular/facade/src/errors.ts
-         * Errors don't use current this, instead they create a new instance.
-         * We have to do forward all of our api to the nativeInstance. */
-        const nativeError = super(message) as any as Error;
-        this._nativeError = nativeError;
+    get name() {
+        return this.constructor.name;
     }
 
-    get message() { return this._nativeError.message; }
-    set message(message) { this._nativeError.message = message; }
-    get name() { return this._nativeError.name; }
-    get stack() { return (this._nativeError as any).stack; }
-    set stack(value) { (this._nativeError as any).stack = value; }
+    toString() {
+        return `${this.name}: ${this.message}`;
+    }
 
 }
