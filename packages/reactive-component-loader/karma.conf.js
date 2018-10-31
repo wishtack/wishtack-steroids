1fc6c52c0b311833daeac86287aaf893567b5ef6
Younes JAAIDI <yjaaidi@gmail.com>
2018-10-31 18:05:53 +0100

    Flattening reactive-component-loader project structure.

--- packages/reactive-component-loader/karma.conf.js
+++ packages/reactive-component-loader/karma.conf.js
@@ -16,7 +16,7 @@ module.exports = function (config) {
       clearContext: false // leave Jasmine Spec Runner output visible in browser
     },
     coverageIstanbulReporter: {
-      dir: require('path').join(__dirname, '../../coverage'),
+      dir: require('path').join(__dirname, 'coverage'),
       reports: ['html', 'lcovonly'],
       fixWebpackSourcePaths: true
     },
@@ -25,7 +25,7 @@ module.exports = function (config) {
     colors: true,
     logLevel: config.LOG_INFO,
     autoWatch: true,
-    browsers: ['Chrome'],
+    browsers: ['ChromeHeadless'],
     singleRun: false
   });
 };
