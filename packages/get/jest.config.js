4eac6a346879097e83895f138af84e8bcacf6e39
Younes JAAIDI <yjaaidi@gmail.com>
2019-02-12 08:31:47 +0100

    chore: Using ts-jest preset.

--- packages/get/jest.config.js
+++ packages/get/jest.config.js
@@ -7,20 +7,7 @@
 'use strict';
 
 module.exports = {
-    "roots": [
-        "<rootDir>/src"
-    ],
-    "transform": {
-        "^.+\\.tsx?$": "ts-jest"
-    },
-    "testRegex": "(/__tests__/.*|(\\.|/)(test|spec))\\.tsx?$",
-    "moduleFileExtensions": [
-        "ts",
-        "tsx",
-        "js",
-        "jsx",
-        "json",
-        "node"
-    ],
+    preset: 'ts-jest',
+    testEnvironment: 'node'
 };
 
