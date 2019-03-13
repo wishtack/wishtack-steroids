d8bd37e8122aa65ec44d49db6107c29736f30e44
Younes JAAIDI <yjaaidi@gmail.com>
2019-03-13 09:59:11 +0100

    Scam schematic.

--- packages/scam/src/scam/index.ts
+++ packages/scam/src/scam/index.ts
@@ -1,9 +1,10 @@
 import { Rule, SchematicContext, Tree } from '@angular-devkit/schematics';
 import moduleSchematic from '@schematics/angular/module';
+import { Schema as ModuleOptions } from '@schematics/angular/module/schema';
 
 // You don't have to export the function as default. You can also have more than one rule factory
 // per file.
-export function scam(_options: {[key: string]: any}): Rule {
+export function scam(_options: ModuleOptions): Rule {
     return (tree: Tree, _context: SchematicContext) => {
         console.log(moduleSchematic);
         return tree;
