6deb1290be8583724410776810bc07311c1b0549
Younes JAAIDI <yjaaidi@gmail.com>
2019-03-13 10:10:48 +0100

    Scam schematic.

--- packages/scam/src/scam/index.spec.ts
+++ packages/scam/src/scam/index.spec.ts
@@ -8,6 +8,33 @@ const collectionPath = path.join(__dirname, '../collection.json');
 
 describe('scam', () => {
 
+    let appTree: Tree;
+
+    beforeEach(() => {
+
+        const workspaceOptions = {
+            name: 'workspace',
+            newProjectRoot: 'projects',
+            version: '6.0.0',
+        };
+
+        const appOptions = {
+            name: 'bar',
+            inlineStyle: false,
+            inlineTemplate: false,
+            routing: false,
+            skipTests: false,
+            skipPackageJson: false,
+        };
+        const schematicRunner = new SchematicTestRunner(
+            '@schematics/angular',
+            require.resolve('@schematics/angular/collection.json'),
+        );
+        appTree = schematicRunner.runSchematic('workspace', workspaceOptions);
+        appTree = schematicRunner.runSchematic('application', appOptions, appTree);
+
+    });
+
     it('should create a module with a component in the same directory', () => {
 
         const runner = new SchematicTestRunner('schematics', collectionPath);
