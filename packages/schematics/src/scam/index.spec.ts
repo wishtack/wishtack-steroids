8ba924084a3ebc4abcdef184cb4cb60bba01eb67
Younes JAAIDI <yjaaidi@gmail.com>
2019-03-13 11:53:29 +0100

    Scam schematic.

--- packages/schematics/src/scam/index.spec.ts
+++ packages/schematics/src/scam/index.spec.ts
@@ -50,7 +50,7 @@ describe('scam', () => {
         const moduleContent = tree.readContent('/projects/wishtack/src/app/hello-world/hello-world.module.ts');
         expect(moduleContent).toMatch(/import.*HelloWorldComponent.*from '.\/hello-world.component'/);
         expect(moduleContent).toMatch(/declarations:\s*\[HelloWorldComponent\]/m);
-        // expect(moduleContent).toMatch(/exports:\s*\[\s*HelloWorldComponent/m);
+        expect(moduleContent).toMatch(/exports:\s*\[\s*HelloWorldComponent/m);
 
     });
 
