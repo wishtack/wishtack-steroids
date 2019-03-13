2d3762efe14c4885513b28f9f8a73b15fccc10a3
Younes JAAIDI <yjaaidi@gmail.com>
2019-03-13 10:20:46 +0100

    Scam schematic.

--- packages/scam/src/scam/index.ts
+++ packages/scam/src/scam/index.ts
@@ -1,9 +1,11 @@
-import { Rule, SchematicContext, Tree } from '@angular-devkit/schematics';
+import { chain, Rule, SchematicContext, Tree } from '@angular-devkit/schematics';
 import moduleSchematic from '@schematics/angular/module';
 import { Schema } from '@schematics/angular/module/schema';
 
 export function scam(options: Schema): Rule {
     return (tree: Tree, context: SchematicContext) => {
-        return moduleSchematic(options)(tree, context);
+        return chain([
+            moduleSchematic(options)
+        ])(tree, context);
     };
 }
