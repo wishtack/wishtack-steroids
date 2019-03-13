0fe0f22b8bb4709382c71045f01cada5427c1f4c
Younes JAAIDI <yjaaidi@gmail.com>
2019-03-13 10:46:41 +0100

    Scam schematic.

--- packages/scam/src/scam/index.spec.ts
+++ packages/scam/src/scam/index.spec.ts
@@ -3,10 +3,10 @@ import { SchematicTestRunner } from '@angular-devkit/schematics/testing';
 import * as path from 'path';
 
 
-const collectionPath = path.join(__dirname, '../collection.json');
-
 
 describe('scam', () => {
+
+    const collectionPath = path.join(__dirname, '../collection.json');
 
     let appTree: Tree;
 
