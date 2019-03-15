f6e57e0897909591a5cd59c7c67069be5a2c05d3
Younes JAAIDI <yjaaidi@gmail.com>
2019-03-15 16:36:39 +0100

    @wishtack/schematics:scam: Merge module and component as default behavior.

--- packages/schematics/src/scam/index.spec.ts
+++ packages/schematics/src/scam/index.spec.ts
@@ -2,76 +2,87 @@ import { Tree } from '@angular-devkit/schematics';
 import { SchematicTestRunner } from '@angular-devkit/schematics/testing';
 import * as path from 'path';
 
-
-
 describe('scam', () => {
 
-    const collectionPath = path.join(__dirname, '../collection.json');
+    describe('schematic', () => {
 
-    let appTree: Tree;
+        const collectionPath = path.join(__dirname, '../collection.json');
 
-    beforeEach(() => {
+        let appTree: Tree;
 
-        const workspaceOptions = {
-            name: 'workspace',
-            newProjectRoot: 'projects',
-            version: '6.0.0',
-        };
+        beforeEach(() => {
 
-        const appOptions = {
-            name: 'wishtack',
-            inlineStyle: false,
-            inlineTemplate: false,
-            routing: false,
-            skipTests: false,
-            skipPackageJson: false,
-        };
-        const schematicRunner = new SchematicTestRunner(
-            '@schematics/angular',
-            require.resolve('@schematics/angular/collection.json'),
-        );
-        appTree = schematicRunner.runSchematic('workspace', workspaceOptions);
-        appTree = schematicRunner.runSchematic('application', appOptions, appTree);
+            const workspaceOptions = {
+                name: 'workspace',
+                newProjectRoot: 'projects',
+                version: '6.0.0',
+            };
+
+            const appOptions = {
+                name: 'wishtack',
+                inlineStyle: false,
+                inlineTemplate: false,
+                routing: false,
+                skipTests: false,
+                skipPackageJson: false,
+            };
+            const schematicRunner = new SchematicTestRunner(
+                '@schematics/angular',
+                require.resolve('@schematics/angular/collection.json'),
+            );
+            appTree = schematicRunner.runSchematic('workspace', workspaceOptions);
+            appTree = schematicRunner.runSchematic('application', appOptions, appTree);
+
+        });
+
+        it(`WIP: should create module in the component's file`, () => {
+
+            const runner = new SchematicTestRunner('schematics', collectionPath);
+
+            const tree = runner.runSchematic('scam', {
+                name: 'hello-world',
+                project: 'wishtack'
+            }, appTree);
+
+            expect(tree.files).toContain('/projects/wishtack/src/app/hello-world/hello-world.component.ts');
+            expect(tree.files).not.toContain('/projects/wishtack/src/app/hello-world/hello-world.module.ts');
+
+            const component = tree.readContent('/projects/wishtack/src/app/hello-world/hello-world.component.ts');
+            expect(component).toMatch(/export class HelloWorldComponent/);
+            // @todo: expect(component).toMatch(/exports:\s*\[\s*HelloWorldComponent]/m);
+
+        });
+
+        it('should create a module with a component in the same directory', () => {
+
+            const runner = new SchematicTestRunner('schematics', collectionPath);
+
+            const tree = runner.runSchematic('scam', {
+                name: 'views/hello-world',
+                project: 'wishtack',
+                separateModule: true
+            }, appTree);
+
+            expect(tree.files).toContain('/projects/wishtack/src/app/views/hello-world/hello-world.module.ts');
+            expect(tree.files).toContain('/projects/wishtack/src/app/views/hello-world/hello-world.component.ts');
+
+            const moduleContent = tree
+                .readContent('/projects/wishtack/src/app/views/hello-world/hello-world.module.ts');
+            expect(moduleContent).toMatch(/import.*HelloWorldComponent.*from '.\/hello-world.component'/);
+            expect(moduleContent).toMatch(/declarations:\s*\[HelloWorldComponent]/m);
+            expect(moduleContent).toMatch(/exports:\s*\[\s*HelloWorldComponent]/m);
+
+        });
 
     });
 
-    it(`WIP: should create module in the component's file`, () => {
+    describe('_mergeComponentAndModule', () => {
 
-        const runner = new SchematicTestRunner('schematics', collectionPath);
+        xit('should merge component and module', () => {
 
-        const tree = runner.runSchematic('scam', {
-            name: 'hello-world',
-            project: 'wishtack'
-        }, appTree);
-
-        expect(tree.files).toContain('/projects/wishtack/src/app/hello-world/hello-world.component.ts');
-        expect(tree.files).not.toContain('/projects/wishtack/src/app/hello-world/hello-world.module.ts');
-
-        const component = tree.readContent('/projects/wishtack/src/app/hello-world/hello-world.component.ts');
-        expect(component).toMatch(/export class HelloWorldComponent/);
-        // @todo: expect(component).toMatch(/exports:\s*\[\s*HelloWorldComponent]/m);
-
-    });
-
-    it('should create a module with a component in the same directory', () => {
-
-        const runner = new SchematicTestRunner('schematics', collectionPath);
-
-        const tree = runner.runSchematic('scam', {
-            name: 'views/hello-world',
-            project: 'wishtack',
-            separateModule: true
-        }, appTree);
-
-        expect(tree.files).toContain('/projects/wishtack/src/app/views/hello-world/hello-world.module.ts');
-        expect(tree.files).toContain('/projects/wishtack/src/app/views/hello-world/hello-world.component.ts');
-
-        const moduleContent = tree
-            .readContent('/projects/wishtack/src/app/views/hello-world/hello-world.module.ts');
-        expect(moduleContent).toMatch(/import.*HelloWorldComponent.*from '.\/hello-world.component'/);
-        expect(moduleContent).toMatch(/declarations:\s*\[HelloWorldComponent]/m);
-        expect(moduleContent).toMatch(/exports:\s*\[\s*HelloWorldComponent]/m);
+        });
 
     });
 
 });
+
