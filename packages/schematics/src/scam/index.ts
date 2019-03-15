6733149386b0feeeaa406a8f978aa6e770eb963a
Younes JAAIDI <yjaaidi@gmail.com>
2019-03-15 16:49:54 +0100

    @wishtack/schematics:scam: Merge module and component as default behavior.

--- packages/schematics/src/scam/index.ts
+++ packages/schematics/src/scam/index.ts
@@ -11,6 +11,7 @@ export interface ScamOptions extends NgComponentOptions {
  * A function that simply merges component and module, and removes useless imports.
  * @param componentContent
  * @param moduleContent
+ * @deprecated wip
  * @private
  */
 export function _mergeComponentAndModule(componentContent: string, moduleContent: string): string {
@@ -19,6 +20,7 @@ export function _mergeComponentAndModule(componentContent: string, moduleContent
 
 /**
  * Schematics rule factory that merges module into component.
+ * @deprecated wip
  * @private
  */
 export const _mergeModuleIntoComponentFile: (options: ScamOptions) => Rule = (options) => (tree, context) => {
